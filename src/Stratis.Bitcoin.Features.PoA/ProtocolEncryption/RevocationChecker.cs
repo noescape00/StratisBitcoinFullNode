03061fe6866b66e5a1887cc67beb52c1a06d3c41
noescape0 <NoEscape0@ya.ru>
2019-10-30 13:37:53 +0300

    fixed TODO

--- src/Stratis.Bitcoin.Features.PoA/ProtocolEncryption/RevocationChecker.cs
+++ src/Stratis.Bitcoin.Features.PoA/ProtocolEncryption/RevocationChecker.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Net.Http;
-using System.Runtime.CompilerServices;
-using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using CertificateAuthority.Client;
 using Microsoft.Extensions.Logging;
@@ -13,10 +12,6 @@ namespace Stratis.Bitcoin.Features.PoA.ProtocolEncryption
 {
     public class RevocationChecker : IDisposable
     {
-        /*
-        * TODO update cache every 12 hours
-        */
-
         private const string kvRepoKey = "revocedcerts";
 
         private readonly NodeSettings nodeSettings;
@@ -25,28 +20,37 @@ namespace Stratis.Bitcoin.Features.PoA.ProtocolEncryption
 
         protected ILogger logger;
 
-        private HashSet<string> revokedCertsCashe;
+        private HashSet<string> revokedCertsCache;
 
         private Client client;
+
+        private Task cacheUpdatingTask;
+
+        private CancellationTokenSource cancellation;
+
+        private readonly TimeSpan CacheUpdateInterval = TimeSpan.FromHours(12);
 
         public RevocationChecker(NodeSettings nodeSettings, IKeyValueRepository kvRepo, ILoggerFactory loggerFactory)
         {
             this.nodeSettings = nodeSettings;
             this.kvRepo = kvRepo;
             this.logger = loggerFactory.CreateLogger(this.GetType().FullName);
+            this.cancellation = new CancellationTokenSource();
         }
 
         public async Task InitializeAsync()
         {
-            TextFileConfiguration config = nodeSettings.ConfigReader;
+            TextFileConfiguration config = this.nodeSettings.ConfigReader;
             string certificateAuthorityUrl = config.GetOrDefault<string>("caurl", "https://localhost:5001");
 
             this.client = new Client(certificateAuthorityUrl, new HttpClient());
 
-            this.revokedCertsCashe = this.kvRepo.LoadValue<HashSet<string>>(kvRepoKey);
+            this.revokedCertsCache = this.kvRepo.LoadValue<HashSet<string>>(kvRepoKey);
 
-            if (this.revokedCertsCashe == null)
-                await UpdateRevokedCertsCasheAsync().ConfigureAwait(false);
+            if (this.revokedCertsCache == null)
+                await this.UpdateRevokedCertsCacheAsync().ConfigureAwait(false);
+
+            this.cacheUpdatingTask = this.UpdateRevokedCertsCacheContinuouslyAsync();
         }
 
         public async Task<bool> IsCertificateRevokedAsync(string thumbprint)
@@ -54,7 +58,7 @@ namespace Stratis.Bitcoin.Features.PoA.ProtocolEncryption
             // First try to ask CA server directly.
             try
             {
-                var requestModel = new GetCertificateStatusModel() {AsString = false, Thumbprint = thumbprint};
+                var requestModel = new GetCertificateStatusModel() {AsString = true, Thumbprint = thumbprint};
                 string status = await this.client.GetCertificateStatusAsync(requestModel).ConfigureAwait(false);
 
                 return status != "Good";
@@ -64,19 +68,19 @@ namespace Stratis.Bitcoin.Features.PoA.ProtocolEncryption
                 this.logger.LogDebug("Error while checking certificate status: '{0}'.", e.ToString());
 
                 // Use cache.
-                if ((this.revokedCertsCashe != null) && this.revokedCertsCashe.Contains(thumbprint))
+                if ((this.revokedCertsCache != null) && this.revokedCertsCache.Contains(thumbprint))
                     return true;
             }
 
             return false;
         }
 
-        private async Task UpdateRevokedCertsCasheAsync()
+        private async Task UpdateRevokedCertsCacheAsync()
         {
             try
             {
                 ICollection<string> result = await this.client.GetRevokedCertificatesAsync().ConfigureAwait(false);
-                this.revokedCertsCashe = new HashSet<string>(result);
+                this.revokedCertsCache = new HashSet<string>(result);
             }
             catch (Exception e)
             {
@@ -85,10 +89,30 @@ namespace Stratis.Bitcoin.Features.PoA.ProtocolEncryption
             }
         }
 
+        private async Task UpdateRevokedCertsCacheContinuouslyAsync()
+        {
+            while (!this.cancellation.IsCancellationRequested)
+            {
+                try
+                {
+                    await Task.Delay(this.CacheUpdateInterval, this.cancellation.Token);
+                }
+                catch (OperationCanceledException)
+                {
+                    return;
+                }
+
+                await this.UpdateRevokedCertsCacheAsync().ConfigureAwait(false);
+            }
+        }
+
         public void Dispose()
         {
-            if (this.revokedCertsCashe != null)
-                this.kvRepo.SaveValue(kvRepoKey, this.revokedCertsCashe);
+            if (this.revokedCertsCache != null)
+                this.kvRepo.SaveValue(kvRepoKey, this.revokedCertsCache);
+
+            this.cancellation.Cancel();
+            this.cacheUpdatingTask?.GetAwaiter().GetResult();
         }
     }
 }
